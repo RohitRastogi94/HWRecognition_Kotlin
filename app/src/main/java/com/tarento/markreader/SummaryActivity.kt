86889ae65731da3fc4871bdaf5d3e50c56d0ec4b
srini1201 <srinivasan.venkatachalam@tarento.com>
2020-04-01 14:36:48 +0530

    Commit to optimize and handle the error message in the student summary details.

--- app/src/main/java/com/tarento/markreader/SummaryActivity.kt
+++ app/src/main/java/com/tarento/markreader/SummaryActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import com.tarento.markreader.data.ApiClient
 import com.tarento.markreader.data.OCRService
 import com.tarento.markreader.data.SaveOCRRequest
@@ -19,19 +20,19 @@ import kotlinx.android.synthetic.main.activity_summary.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.Exception
 
 class SummaryActivity : AppCompatActivity() {
 
-    var processResult: ProcessResult? = null
-    var checkOCRResponse: CheckOCRResponse? = null
+    private var processResult: ProcessResult? = null
+    private var checkOCRResponse: CheckOCRResponse? = null
     var saveOCRResponse: SaveOCRResponse? = null
-    var totalMarks: Float = 0F
-    var totalMarksSecured: Float = 0F
+    private var totalMarks: Float = 0F
+    private var totalMarksSecured: Float = 0F
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_summary)
-
         val bundle = intent?.extras
         bundle?.let {
             processResult = it.getSerializable("data") as ProcessResult?
@@ -39,16 +40,15 @@ class SummaryActivity : AppCompatActivity() {
             totalMarks = it.getFloat("TotalMarks")
             totalMarksSecured = it.getFloat("TotalMarksSecured")
         }
-        if (checkOCRResponse != null) {
+        checkOCRResponse?.let {
             textSummaryStudentFirstLetter.text =
-                checkOCRResponse!!.data.student_name.first().toString()
-            textSummaryStudentName.text = checkOCRResponse!!.data.student_name
-            textSummaryStudentId.text = checkOCRResponse!!.data.student_code
-            textSummaryTestId.text = checkOCRResponse!!.data.exam_code
-            textTotalMarks.text = String.format("%.2f", this.totalMarks)
-            textTotalMarksSecured.text = String.format("%.2f", this.totalMarksSecured)
+                it.data.student_name.first().toString()
+            textSummaryStudentName.text = it.data.student_name
+            textSummaryStudentId.text = it.data.student_code
+            textSummaryTestId.text = it.data.exam_code
         }
-
+        textTotalMarks.text = String.format("%.2f", this.totalMarks)
+        textTotalMarksSecured.text = String.format("%.2f", this.totalMarksSecured)
 
         buttonEditSummary.setOnClickListener {
             this.finish()
@@ -60,26 +60,18 @@ class SummaryActivity : AppCompatActivity() {
     }
 
     private fun saveOCRData() {
-        var appPreferenceHelper = AppPreferenceHelper(this.applicationContext)
-
-        val apiInterface: OCRService = ApiClient.getClient()!!.create(OCRService::class.java)
+        val appPreferenceHelper = AppPreferenceHelper(this.applicationContext)
+        val apiInterface: OCRService? = ApiClient.createAPIService()
         val saveOCRRequest = SaveOCRRequest(
-            appPreferenceHelper.getExamDate()!!,
-            appPreferenceHelper.getExamCode()!!,
-            appPreferenceHelper.getStudentCode()!!,
-            appPreferenceHelper.getTeacherCode()!!,
+            appPreferenceHelper.getExamDate()?:"",
+            appPreferenceHelper.getExamCode()?:"",
+            appPreferenceHelper.getStudentCode()?:"",
+            appPreferenceHelper.getTeacherCode()?:"",
             checkOCRResponse!!.data.ocr_data
         )
-
-        val hero = apiInterface.saveOCR(saveOCRRequest)
-
-        hero.enqueue(object : Callback<SaveOCRResponse> {
+        apiInterface?.saveOCR(saveOCRRequest)?.enqueue(object : Callback<SaveOCRResponse> {
             override fun onFailure(call: Call<SaveOCRResponse>, t: Throwable) {
-                Log.e(SubjectDetailsFragment.TAG, "onResponse: Failuer", t)
-
-                Toast.makeText(this@SummaryActivity, "Some thing went wrong", Toast.LENGTH_SHORT)
-                //.show()
-
+                Log.e(SubjectDetailsFragment.TAG, "onResponse: Failure", t)
                 ProgressBarUtil.dismissProgressDialog()
             }
 
@@ -89,63 +81,55 @@ class SummaryActivity : AppCompatActivity() {
             ) {
                 Log.d(SubjectDetailsFragment.TAG, "onResponse: ${response.isSuccessful}")
                 ProgressBarUtil.dismissProgressDialog()
-                if (response != null && response.isSuccessful && response.body() != null) {
+                if (response.isSuccessful && response.body() != null) {
                     Log.d(SubjectDetailsFragment.TAG, "onResponse: ${response.body()}")
                     saveOCRResponse = response.body()
-
                     saveOCRResponse?.let {
-                        if (saveOCRResponse != null) {
-                            if (saveOCRResponse!!.http.status == 200) {
-                                val summaryMessage =
-                                    AlertDialog.Builder(this@SummaryActivity, R.style.DialogTheme).create()
-                                summaryMessage.setTitle("Data Summary")
-                                summaryMessage.setMessage("Saved Successfully")
-                                summaryMessage.setCancelable(false)
-
-                                summaryMessage.setButton(
-                                    AlertDialog.BUTTON_POSITIVE,
-                                    "Ok"
-                                ) { dialog, which ->
-                                    val intent = Intent(
-                                        this@SummaryActivity,
-                                        IndexActivity::class.java
-                                    )
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                                    startActivity(intent)
-                                    finish()
-                                }
-                                summaryMessage.show()
-                            } else {
-                                Toast.makeText(
-                                    this@SummaryActivity,
-                                    "Some thing went wrong",
-                                    Toast.LENGTH_SHORT
-                                )
-                                    .show()
-
+                        if (it.http.status == 200) {
+                            val summaryMessage =
+                                AlertDialog.Builder(this@SummaryActivity, R.style.DialogTheme)
+                                    .create()
+                            summaryMessage.setTitle(getString(R.string.data_summary))
+                            summaryMessage.setMessage(getString(R.string.saved_successfully))
+                            summaryMessage.setCancelable(false)
+                            summaryMessage.setButton(
+                                AlertDialog.BUTTON_POSITIVE,
+                                getString(R.string.ok)
+                            ) { _, _ ->
+                                val intent = Intent(this@SummaryActivity, IndexActivity::class.java)
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                startActivity(intent)
+                                finish()
                             }
+                            summaryMessage.show()
                         } else {
                             Toast.makeText(
-                                this@SummaryActivity,
-                                "Some thing went wrong",
+                                this@SummaryActivity, getString(R.string.something_went_wrong),
                                 Toast.LENGTH_SHORT
-                            )
-                                .show()
-
+                            ).show()
                         }
-
                     }
                 } else {
+                    var errorMessage = getString(R.string.something_went_wrong)
+                    try {
+                        val saveOCRErrorResponseOj = Gson().fromJson<SaveOCRResponse>(
+                            response.errorBody()?.string() ?: "",
+                            SaveOCRResponse::class.java
+                        )
+                        saveOCRErrorResponseOj?.let {
+                            errorMessage = it.why
+                        }
+                    } catch (ex: Exception) {
+                        ex.printStackTrace()
+                    }
                     Toast.makeText(
                         this@SummaryActivity,
-                        "Some thing went wrong",
+                        errorMessage,
                         Toast.LENGTH_SHORT
-                    )
-                        .show()
+                    ).show()
                 }
             }
-
         })
     }
 }
